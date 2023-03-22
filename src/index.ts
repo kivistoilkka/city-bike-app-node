c4514e16b91a59c1b84878d7eb860adf35ac4602
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-03-22 23:04:37 +0200

    Sequelize added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Sequelize } from 'sequelize';
 import express from 'express';
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+if (!process.env.DATABASE_URL || !isString(process.env.DATABASE_URL)) {
+  throw new Error('Database address incorrect or missing');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 const app = express();
 app.use(express.json());
 
@@ -9,6 +22,19 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const main = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequelize.close();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log('Unable to connect to the database', error);
+  }
+
+};
+
+void main();
